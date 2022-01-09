15e3a6af0c8e479663455a066e56526722b6359d
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2022-01-09 12:02:42 +0330

    End 0f 9

--- dom.js
+++ dom.js
@@ -16,3 +16,28 @@ function childes() {
   //alert(document.getElementById("d1").parentNode.parentNode.firstChild)
   alert(document.getElementById("d1").firstChild.nextSibling.nodeType)
 }
+
+function addNewLink() {
+  var newLink = document.createElement("a")
+  var linkText = document.createTextNode("Link 3")
+  //newLink.appendChild(linkText)
+
+  newLink.innerHTML = "Link 3"
+  newLink.href="#"
+  newLink.onclick = function () {alert()}
+  document.getElementById("d1").appendChild(newLink)
+  var target = document.getElementById('d1').getElementsByTagName('a')[0]
+  //document.getElementById("d1").insertBefore(newLink, target)
+  //document.getElementById("d1").replaceChild(newLink, target)
+}
+
+function removeLink() {
+  var parent = document.getElementById("d1")
+  var child = document.getElementById("d1").getElementsByTagName("a")[0]
+  parent.removeChild(child)
+}
+
+function ff() {
+  alert(document.documentElement.innerHTML)
+  alert(document.body)
+}
