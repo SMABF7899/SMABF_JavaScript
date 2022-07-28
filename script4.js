8feca3fce64264488e83b77431b17c309cbf53b2
SMABF7899 <smabanifatemi@gmail.com>
2022-07-28 11:42:03 +0430

    ntroduction to rototype

--- script4.js
+++ script4.js
@@ -45,3 +45,52 @@ var car3 = new Car("BMW", "Series 7", 2022, 300);
 console.log(car3)
 console.log(car3 instanceof Object)
 console.log("=========================================================================================================")
+console.log(car3.constructor)
+console.log(myCar.constructor)
+
+Car.prototype.fun = function () {
+    return "fun";
+}
+console.log(car3.fun())
+
+Car.prototype.details = function (color, wheels) {
+    this.color = color;
+    this.wheels = wheels;
+    return "Color : " + color + "\nWheels : " + wheels
+}
+
+console.log(car3.details("red", 4))
+
+Object.prototype.sum = function () {
+    return "sum"
+}
+
+console.log(car3.sum())
+console.log(myCar.sum())
+
+function Person (name) {
+    this.name = name;
+}
+
+Person.prototype.sum = function () {
+    return "sum + 1"
+}
+
+var ali = new Person("ali");
+console.log(ali.sum())
+
+var list = new Array(1, 2, 3, 4, 5, 6);
+console.log(list)
+console.log(list.constructor)
+console.log(list.sum())
+console.log(list.hasOwnProperty('9'))
+
+var iphone = new String("iPhone X");
+console.log(iphone.constructor)
+
+var num = new Number(1234);
+console.log(num.constructor)
+
+var status = new Boolean(true);
+console.log(status.constructor)
+console.log("=========================================================================================================")
