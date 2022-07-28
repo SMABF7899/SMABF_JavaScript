7170216890194fc8ec98fcf8dd1a2deff7a5cb2f
SMABF7899 <smabanifatemi@gmail.com>
2022-07-28 17:27:15 +0430

    Math calculation functions

--- script6.js
+++ script6.js
@@ -12,3 +12,18 @@ console.log(new Date(new Date(Date.parse("2022/7/30"))))
 console.log(new Date(Date.parse("2022/7/30")).getHours());
 console.log(new Date(new Date(Date.parse("2022/7/30")).setHours(12)));
 console.log(new Date(new Date(Date.parse("2022/7/30")).setHours(48)));
+console.log("=========================================================================================================")
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+console.log(Math.PI);
+console.log(Math.abs(-50));
+console.log(Math.floor(2.8));
+console.log(Math.random());
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+console.log(getRandomInt(100));
+console.log(Math.round(1.4));
+console.log(Math.round(1.6));
+console.log(Math.pow(3, 7));
+console.log("=========================================================================================================")
