738059679706255bc0caf000f11daab38b6ff253
SMABF7899 <smabanifatemi@gmail.com>
2022-07-30 12:28:15 +0430

    Introduction to Dialogs

---
+++ script8.js
@@ -0,0 +1,9 @@
+'use strict'
+try {
+    alert("Hello");
+    console.log(confirm("Are you ok ?"));
+    console.log(prompt("what's your name ?"));
+} catch (error) {
+    console.log(error);
+}
+console.log("=========================================================================================================")
