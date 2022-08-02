d7f7bc80999bcffde536b606396b23f9eaf12576
SMABF7899 <smabanifatemi@gmail.com>
2022-08-02 13:52:34 +0430

    Getting to know Event and Event Handlers

--- script8.js
+++ script8.js
@@ -7,3 +7,27 @@ try {
     console.log(error);
 }
 console.log("=========================================================================================================")
+// https://developer.mozilla.org/en-US/docs/Web/API/Event
+try {
+    var title = document.querySelector("#title");
+    title.onclick = function () {
+        console.log("click");
+    }
+    title.onmouseenter = function () {
+        console.log("click enter");
+    }
+    title.ondblclick = function () {
+        console.log("click 2");
+    }
+    title.onclick = function (event) {
+        event.target.style.backgroundColor = "red";
+    }
+    title.onmouseenter = function (event) {
+        event.target.style.backgroundColor = "green";
+    }
+    title.onmouseout = function (event) {
+        event.target.style.backgroundColor = "blue";
+    }
+} catch (error) {
+    console.log(error);
+}
