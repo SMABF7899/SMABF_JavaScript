15a1e63b9e6b1ad06f188e4c6fc177c7a4edc7e8
SMABF7899 <smabanifatemi@gmail.com>
2022-08-02 17:55:59 +0430

    Learning how to work with AJAX request - POST request

--- script9.js
+++ script9.js
@@ -28,3 +28,27 @@ try {
     console.log(error);
 }
 console.log("=========================================================================================================")
+try {
+    var ajax2 = new XMLHttpRequest();
+    var data = {
+        username : "ma.banifatemi",
+        email : "smabanifatemi78@gmail.com"
+    }
+    ajax2.open('POST', 'https://jsonplaceholder.typicode.com/users');
+    ajax2.setRequestHeader('Content-type', "application/json; charset=UTF-8");
+    ajax2.addEventListener('load', function () {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 201) {
+                console.log('done');
+                var data = JSON.parse(this.responseText);
+                console.log(data.username);
+                console.log(data.email);
+            } else
+                console.log(this.status);
+        }
+    })
+    ajax2.send(JSON.stringify(data))
+} catch (error) {
+    console.log(error)
+}
+console.log("=========================================================================================================")
